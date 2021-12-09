b2d266e6bfe0d5d6fdad3b3f70436a9b4e333a57
la.kripchenko <s.kripchenko@kernel.ua>
2021-12-09 21:42:48 +0200

    aaaaa

---
+++ main.py
@@ -0,0 +1,9 @@
+class human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+first_human = human('Sergii', 32)
+second_human = human('Lidia', 60)
+# print(first_human.name)
+# print(second_human.name)
+# print(second_human.age)
